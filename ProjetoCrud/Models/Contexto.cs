250759b4fb06305766c01f428c3bea60b52ecff3
Usuário <Usurio@DESKTOP-E6AHAVI>
2024-01-28 19:56:26 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoCrud/Models/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoCrud.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto( DbContextOptions<Contexto> options ) : base(options)
+        {
+
+        }
+        public DbSet<Empresa> Empresa {  get; set; }
+    }
+}
---
+++ ProjetoCrud/Models/Empresa.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoCrud.Models
+{
+    public class Empresa
+    {   
+        public int Id { get; set; }
+        [StringLength(20, ErrorMessage="A Razão Social deve ter no máximo 20 caracteres")]
+        public string RazaoSocial { get; set; }
+    }
+}
